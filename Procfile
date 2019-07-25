ae572186c47e222d493fe0e1ec6483f20f6c474b
redcrix <contact@redcrix.com>
2019-07-25 15:12:59 +0530

    first commit

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 web: gunicorn app:app
-=======
-web: gunicorn app:app
->>>>>>> 4933669d5ed7d459840babde07b901eed607daad
--- app.py
+++ app.py
@@ -40,6 +40,5 @@ def upload_file():
 
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 33507))
-	app.run(host='0.0.0.0', port=port)
+	app.run(debug=True)
 
