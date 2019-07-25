12a39521d79e7fbdda46a1076012f0da60bfdea7
redcrix <contact@redcrix.com>
2019-07-25 13:41:43 +0530

    first commit

--- app.py
+++ app.py
@@ -37,8 +37,9 @@ def upload_file():
 
 		return json.dumps({"dob": a})
 
-port = int(os.environ.get('PORT', 5000))
+
 
 if __name__ == "__main__":
+	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
 
