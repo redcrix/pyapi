e6329fe107dca3733e1f9ad827b2bc05f77e80fc
redcrix <contact@redcrix.com>
2019-07-25 11:29:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, json
+import image_slicer
+import os
+from PIL import Image
+import pytesseract
+import cv2
+import re
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def upload_file():
+	if request.method == 'POST':
+		#print(request.files)
+		if 'file' not in request.files:
+			return "No file found"
+		file = request.files['file']
+		file.save("a.jpg")
+
+		text=[]
+		image_slicer.slice("a.jpg", 10)
+
+		for f in os.listdir("."):
+			if '.png' in f:
+				image = cv2.imread(f)
+				t = pytesseract.image_to_string(image, lang='eng')
+				text.append(t)
+
+		a = ""
+		for i in range(len(text)):
+			try:
+				match = re.search(r'(\d+/\d+/\d+)', text[i]).group(1)
+				a = match
+			except:
+				pass
+
+		return json.dumps({"dob": a})
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
